e3566fcd1ff50e532c3cdf310c9fe1d9a7a1d20c
MdTanvir-ICE <67551627+MdTanvir-ICE@users.noreply.github.com>
2021-03-28 22:15:19 +0600

    Update graphAlgo.cpp

--- graphAlgo.cpp
+++ graphAlgo.cpp
@@ -20,6 +20,56 @@ void dfs ( int node )
  
 }
 
+.........................
+	2D  DFS
+.........................
+int vis[1001][1001];
+int ara[1001][1001];
+int n,m;
+
+bool isValid (int x, int y){
+    if(x < 1 || x>n || y<1 || y>m)return false;
+
+    if(vis[x][y]== true || ara[x][y]==false)return false;
+
+    return true;
+}
+
+int dx[ ] ={-1,0,1,0};
+int dy[ ] ={0,1,0,-1};
+void dfs( int x , int y){
+
+
+ vis[x][y] = 1;
+
+ cout<<x<<" "<<y<<endl;
+
+   for(int i=0;i<4;i++){
+     if(isValid(x+dx[i], y+dy[i]))
+     dfs( x+dx[i], y+dy[i] );
+    }
+   
+}
+
+int main( ){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>ara[i][j];
+        }
+    }
+    int cc = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(vis[i][j]==false&&ara[i][j]==true){
+                dfs(i,j),cc++;
+            }
+        }
+    }
+    cout<<"CC "<<cc<<endl;
+   
+}
+
 
 .........................
 	  DISTANCE
@@ -168,3 +218,66 @@ void BFS(int src){
         }
     }
 }
+     
+.........................
+	  2D BFS
+..........................
+     
+int dist[1001][1001];
+int n,m;
+int vis[1001][1001];
+
+bool isValid(int x, int y){
+    if(x<1 || x > n || y < 1 || y>m)return false;
+    if(vis[x][y]==1)return false;
+
+    return true;
+}
+int dx[ ] = {-1, 0, 1, 0};
+int dy[ ] = {0, 1, 0, -1};
+
+void bfs(int srcX, int srcY){
+
+    queue< pair<int,int> > q;
+
+    q.push({srcX, srcY});
+    vis[srcX][srcY] =1;
+    dist[srcX][srcY] =0;
+
+   while(!q.empty()){
+       int currX = q.front().first;
+       int currY = q.front().second;
+       q.pop();
+    
+       for(int i=0;i<4;i++){   
+            if(isValid(currX+dx[i],currY+dy[i])){
+                int newX = currX + dx[i];
+                int newY = currY + dy[i];
+                
+                q.push({newX,newY});
+                vis[newX][newY] = 1;
+                dist[newX][newY] = dist[currX][currY] + 1;
+             }
+        }
+
+    }
+
+      cout<<"distance array\n";
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++)
+              cout<<dist[i][j]<<" ";
+
+              cout<<endl;
+
+      }
+
+}
+
+int main(){
+    int x,y;
+    cin>>n>>m;
+    cin>>x>>y;
+    bfs(x,y);
+}
+     
+
